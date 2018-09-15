581535e5e916f0151684a2509b4d6c28e7b118a2
alissar <alissar@alissars-MacBook-Pro.local>
2018-09-15 15:47:38 +0100

    Comonication work

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import firebase from 'firebase';
+import ImageGallery from './components/gallery/ImageGallery';
+// import logo from './images/logo.png';
 
-class App extends Component {
+const classNames = require('./App.css');
+
+export default class App extends React.Component {
+
+  constructor() {
+    super();
+    //In React, components have state!
+    //React state: If change, the component will be reloaded! (Interesting)
+    this.state = {
+      user: null,
+      images: []
+    };
+  }
+
+  //Life cicle method launched when component is rendered in DOM (Firebase Listener).
+  componentWillMount() { 
+    firebase.auth().onAuthStateChanged(user => {
+      //Awesome ES6 Syntax: If user == new user, will not change and eoc will be overwrited.
+      this.setState({ user });
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className={classNames.app}>
+        {/* <header className={classNames.appHeader}>
+          <img src={ logo } className={classNames.appLogo} alt="logo" /> */}
+          <h1 className={classNames.appTitle}>Khaled & Onneil & Justine</h1>
+        {/* </header> */}
+        <div className={classNames.appIntro}>
+          <ImageGallery user={ this.state.user } />
+        </div>
       </div>
     );
   }
 }
-
-export default App;
