639f55c1e1a6a49491c1f404739f439d0ccda472
alissar <alissar@alissars-mbp.home>
2018-09-16 23:46:59 +0100

    Left

--- src/index.js
+++ src/index.js
@@ -6,12 +6,6 @@ import App from './App';
 
 //Firebase configuration
 firebase.initializeApp({
-    apiKey: "AIzaSyA0CjkXYKX3FGW_HQYFcATN1_U2OF80dWM",
-    authDomain: "communication-994db.firebaseapp.com",
-    databaseURL: "https://communication-994db.firebaseio.com",
-    projectId: "communication-994db",
-    storageBucket: "communication-994db.appspot.com",
-    messagingSenderId: "680453452319"
 
     });
 
